fc0cb900ee40fa6457f91502033aebf043689d71
Roderic Russell <rrr5458@gmail.com>
2026-03-02 23:08:04 -0600

    shape box

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,34 +1,46 @@
 import React from "react";
 import "./../styles/Hero.css";
-import imgSroucePng from "../assets/shapes_hero_image.png";
+import bgImg from "../assets/shapes_hero_image.png";
+import triangleImg from "../assets/yellow-triangle.png";
+import orangeImg from "../assets/orange-circle.png";
 import GradientBackground from "./GradientBackground";
 
 const Hero: React.FC = () => {
   return (
     <section className="hero-section">
       <div className="image-background">
-        <img
-          src={imgSroucePng}
-          alt="Hero Background"
-          className="hero-bg-img"
-        />
+        <img src={bgImg} alt="Hero Background" className="hero-bg-img" />
       </div>
       <div className="content-container-main">
         <div className="content-container-one">
+          <img 
+                src={triangleImg} 
+                alt="Yellow triangle" 
+                className="hero-shape shape-tl" 
+              />
           <div className="hero-title-one-container">
-            <h2 className="hero-title-one">AI agents for magical customer experiences</h2>
-            <span className="hero-sub-one">For something like an industry and speciality for that</span>
+            <h2 className="hero-title-one">
+              AI agents for magical customer experiences
+            </h2>
+              <img 
+                src={orangeImg} 
+                alt="Orange circle"
+                height="300"
+                width="300" 
+                style={{position: "absolute", bottom: 50}}
+                className="shape-br" 
+              />
+            <span className="hero-sub-one">
+              For something like an industry and speciality for that
+            </span>
           </div>
+
           <div className="hero-video-container">
-            <video
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="hero-video"
-            >
-              <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
+            <video autoPlay loop muted playsInline className="hero-video">
+              <source
+                src="https://www.w3schools.com/html/mov_bbb.mp4"
+                type="video/mp4"
+              />
             </video>
           </div>
         </div>
@@ -42,9 +54,8 @@ const Hero: React.FC = () => {
           <div className="hero-gradient-overlay">
             <GradientBackground />
           </div>
+        </div>
       </div>
-      </div>
-
     </section>
   );
 };
